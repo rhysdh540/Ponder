0c2ff21cf4bb12230f7d25601e3887dd69da1dfd
rdh <me@rdh540.dev>
2026-05-11 18:55:38 -0400

    rewrite the clustering algorithm

--- common/src/main/java/net/createmod/catnip/outliner/BlockClusterOutline.java
+++ common/src/main/java/net/createmod/catnip/outliner/BlockClusterOutline.java
@@ -1,7 +1,9 @@
 package net.createmod.catnip.outliner;
 
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,7 +13,8 @@ import org.joml.Vector4f;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 
-import net.createmod.catnip.data.Iterate;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
 import net.createmod.catnip.render.BindableTexture;
 import net.createmod.catnip.render.PonderRenderTypes;
 import net.createmod.catnip.render.SuperRenderTypeBuffer;
@@ -21,6 +24,8 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.Direction.Axis;
 import net.minecraft.core.Direction.AxisDirection;
 import net.minecraft.world.phys.Vec3;
+
+import javax.annotation.Nullable;
 
 public class BlockClusterOutline extends Outline {
 
@@ -37,7 +42,7 @@ public class BlockClusterOutline extends Outline {
 	public BlockClusterOutline(Iterable<BlockPos> positions) {
 		this.positions = positions;
 		cluster = new Cluster();
-		positions.forEach(cluster::include);
+		cluster.includeAll(positions);
 	}
 
 	public boolean isSameSelection(Iterable<BlockPos> positions) {
@@ -184,6 +189,7 @@ public class BlockClusterOutline extends Outline {
 
 	private static class Cluster {
 
+		@Nullable
 		private BlockPos anchor;
 		private final Map<MergeEntry, AxisDirection> visibleFaces;
 		private final Set<MergeEntry> visibleEdges;
@@ -191,57 +197,191 @@ public class BlockClusterOutline extends Outline {
 		public Cluster() {
 			visibleEdges = new HashSet<>();
 			visibleFaces = new HashMap<>();
+			anchor = null;
 		}
 
 		public boolean isEmpty() {
 			return anchor == null;
 		}
 
-		public void include(BlockPos pos) {
-			if (anchor == null)
-				anchor = pos;
+		public void includeAll(Iterable<BlockPos> positions) {
+			Iterator<BlockPos> iterator = positions.iterator();
+			if (!iterator.hasNext())
+				return;
 
-			pos = pos.subtract(anchor);
+			anchor = iterator.next();
 
-			// 6 FACES
-			for (Axis axis : Iterate.axes) {
-				Direction direction = Direction.get(AxisDirection.POSITIVE, axis);
-				for (int offset : Iterate.zeroAndOne) {
-					MergeEntry entry = new MergeEntry(axis, pos.relative(direction, offset));
-					if (visibleFaces.remove(entry) == null)
-						visibleFaces.put(entry, offset == 0 ? AxisDirection.NEGATIVE : AxisDirection.POSITIVE);
-				}
+			IntList xs = new IntArrayList();
+			IntList ys = new IntArrayList();
+			IntList zs = new IntArrayList();
+
+			xs.add(0);
+			ys.add(0);
+			zs.add(0);
+
+			int minX = 0, minY = 0, minZ = 0;
+			int maxX = 0, maxY = 0, maxZ = 0;
+
+			int anchorX = anchor.getX();
+			int anchorY = anchor.getY();
+			int anchorZ = anchor.getZ();
+
+			while (iterator.hasNext()) {
+				BlockPos pos = iterator.next();
+				int x = pos.getX() - anchorX;
+				int y = pos.getY() - anchorY;
+				int z = pos.getZ() - anchorZ;
+
+				xs.add(x);
+				ys.add(y);
+				zs.add(z);
+
+				minX = Math.min(minX, x);
+				minY = Math.min(minY, y);
+				minZ = Math.min(minZ, z);
+				maxX = Math.max(maxX, x);
+				maxY = Math.max(maxY, y);
+				maxZ = Math.max(maxZ, z);
 			}
 
-			// 12 EDGES
-			for (Axis axis : Iterate.axes) {
-				for (Axis axis2 : Iterate.axes) {
-					if (axis == axis2)
-						continue;
-					for (Axis axis3 : Iterate.axes) {
-						if (axis == axis3)
-							continue;
-						if (axis2 == axis3)
+			int xOffset = -minX;
+			int yOffset = -minY;
+			int zOffset = -minZ;
+			int sizeX = maxX - minX + 1;
+			int sizeY = maxY - minY + 1;
+			int sizeZ = maxZ - minZ + 1;
+
+			BitSet occupancy = new BitSet(sizeX * sizeY * sizeZ);
+			for (int i = 0; i < xs.size(); i++) {
+				int x = xs.getInt(i) + xOffset;
+				int y = ys.getInt(i) + yOffset;
+				int z = zs.getInt(i) + zOffset;
+				occupancy.set(index(x, y, z, sizeX, sizeY));
+			}
+
+			buildFaces(occupancy, sizeX, sizeY, sizeZ, xOffset, yOffset, zOffset);
+			buildEdges(occupancy, sizeX, sizeY, sizeZ, xOffset, yOffset, zOffset);
+		}
+
+		private void buildFaces(BitSet occupancy, int sizeX, int sizeY, int sizeZ, int xOffset, int yOffset, int zOffset) {
+			for (int x = 0; x <= sizeX; x++) {
+				for (int y = 0; y < sizeY; y++) {
+					for (int z = 0; z < sizeZ; z++) {
+						boolean negativeOccupied = isOccupied(occupancy, x - 1, y, z, sizeX, sizeY, sizeZ);
+						boolean positiveOccupied = isOccupied(occupancy, x, y, z, sizeX, sizeY, sizeZ);
+						if (negativeOccupied == positiveOccupied)
 							continue;
 
-						Direction direction = Direction.get(AxisDirection.POSITIVE, axis2);
-						Direction direction2 = Direction.get(AxisDirection.POSITIVE, axis3);
-
-						for (int offset : Iterate.zeroAndOne) {
-							BlockPos entryPos = pos.relative(direction, offset);
-							for (int offset2 : Iterate.zeroAndOne) {
-								entryPos = entryPos.relative(direction2, offset2);
-								MergeEntry entry = new MergeEntry(axis, entryPos);
-								if (!visibleEdges.remove(entry))
-									visibleEdges.add(entry);
-							}
-						}
+						addFace(Axis.X, x - xOffset, y - yOffset, z - zOffset,
+							negativeOccupied ? AxisDirection.POSITIVE : AxisDirection.NEGATIVE);
 					}
-
-					break;
 				}
 			}
 
+			for (int y = 0; y <= sizeY; y++) {
+				for (int x = 0; x < sizeX; x++) {
+					for (int z = 0; z < sizeZ; z++) {
+						boolean negativeOccupied = isOccupied(occupancy, x, y - 1, z, sizeX, sizeY, sizeZ);
+						boolean positiveOccupied = isOccupied(occupancy, x, y, z, sizeX, sizeY, sizeZ);
+						if (negativeOccupied == positiveOccupied)
+							continue;
+
+						addFace(Axis.Y, x - xOffset, y - yOffset, z - zOffset,
+							negativeOccupied ? AxisDirection.POSITIVE : AxisDirection.NEGATIVE);
+					}
+				}
+			}
+
+			for (int z = 0; z <= sizeZ; z++) {
+				for (int x = 0; x < sizeX; x++) {
+					for (int y = 0; y < sizeY; y++) {
+						boolean negativeOccupied = isOccupied(occupancy, x, y, z - 1, sizeX, sizeY, sizeZ);
+						boolean positiveOccupied = isOccupied(occupancy, x, y, z, sizeX, sizeY, sizeZ);
+						if (negativeOccupied == positiveOccupied)
+							continue;
+
+						addFace(Axis.Z, x - xOffset, y - yOffset, z - zOffset,
+							negativeOccupied ? AxisDirection.POSITIVE : AxisDirection.NEGATIVE);
+					}
+				}
+			}
+		}
+
+		private void buildEdges(BitSet occupancy, int sizeX, int sizeY, int sizeZ, int xOffset, int yOffset, int zOffset) {
+			for (int x = 0; x < sizeX; x++) {
+				for (int y = 0; y <= sizeY; y++) {
+					for (int z = 0; z <= sizeZ; z++) {
+						int occupiedCount = 0;
+						if (isOccupied(occupancy, x, y - 1, z - 1, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+						if (isOccupied(occupancy, x, y - 1, z, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+						if (isOccupied(occupancy, x, y, z - 1, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+						if (isOccupied(occupancy, x, y, z, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+
+						if ((occupiedCount & 1) != 0)
+							addEdge(Axis.X, x - xOffset, y - yOffset, z - zOffset);
+					}
+				}
+			}
+
+			for (int y = 0; y < sizeY; y++) {
+				for (int x = 0; x <= sizeX; x++) {
+					for (int z = 0; z <= sizeZ; z++) {
+						int occupiedCount = 0;
+						if (isOccupied(occupancy, x - 1, y, z - 1, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+						if (isOccupied(occupancy, x, y, z - 1, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+						if (isOccupied(occupancy, x - 1, y, z, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+						if (isOccupied(occupancy, x, y, z, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+
+						if ((occupiedCount & 1) != 0)
+							addEdge(Axis.Y, x - xOffset, y - yOffset, z - zOffset);
+					}
+				}
+			}
+
+			for (int z = 0; z < sizeZ; z++) {
+				for (int x = 0; x <= sizeX; x++) {
+					for (int y = 0; y <= sizeY; y++) {
+						int occupiedCount = 0;
+						if (isOccupied(occupancy, x - 1, y - 1, z, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+						if (isOccupied(occupancy, x, y - 1, z, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+						if (isOccupied(occupancy, x - 1, y, z, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+						if (isOccupied(occupancy, x, y, z, sizeX, sizeY, sizeZ))
+							occupiedCount++;
+
+						if ((occupiedCount & 1) != 0)
+							addEdge(Axis.Z, x - xOffset, y - yOffset, z - zOffset);
+					}
+				}
+			}
+		}
+
+		private void addFace(Axis axis, int x, int y, int z, AxisDirection axisDirection) {
+			visibleFaces.put(new MergeEntry(axis, new BlockPos(x, y, z)), axisDirection);
+		}
+
+		private void addEdge(Axis axis, int x, int y, int z) {
+			visibleEdges.add(new MergeEntry(axis, new BlockPos(x, y, z)));
+		}
+
+		private static int index(int x, int y, int z, int sizeX, int sizeY) {
+			return x + sizeX * (y + sizeY * z);
+		}
+
+		private static boolean isOccupied(BitSet occupancy, int x, int y, int z, int sizeX, int sizeY, int sizeZ) {
+			if (x < 0 || y < 0 || z < 0 || x >= sizeX || y >= sizeY || z >= sizeZ)
+				return false;
+			return occupancy.get(index(x, y, z, sizeX, sizeY));
 		}
 
 	}
